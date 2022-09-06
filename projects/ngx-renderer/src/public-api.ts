53cbaf5a833e5ecde76a2d1ad4d37d713e535565
Yuvaraj Arumugam <yuvaraj.a@concord.net>
2022-09-06 16:18:15 +0530

    dashboard

--- projects/ngx-renderer/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-renderer
- */
-
-export * from './lib/ngx-renderer.service';
-export * from './lib/ngx-renderer.component';
-export * from './lib/ngx-renderer.module';
